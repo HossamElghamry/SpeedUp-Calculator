120aef1e90f2fa43a9919755a996eb00fa9b81cf
HossamElghamry <ghamry_97@hotmail.com>
2019-01-15 00:37:01 +0200

    Main

--- lib/main.dart
+++ lib/main.dart
@@ -109,7 +109,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white)),
                 labelStyle: TextStyle(color: Colors.white,fontSize: 14),
                 labelText: "Serial Portion Percentage"
-              ),
+              ), 
               keyboardType: TextInputType.number,
               onFieldSubmitted: (String d){
                 setState(() {
